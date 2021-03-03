75f289fd584a017b95117be23f83b1d9f13611bd
IkezoeMakoto <150283t@gmail.com>
2021-03-04 00:14:59 +0900

    各実装パターンを分けて、件数を増やした

--- app/src/index2.php
+++ app/src/index2.php
@@ -7,12 +7,23 @@ use App\User\Repository as UserRepository;
 $todoRepo = new TodoRepository();
 
 $todos = $todoRepo->getAll(['\App\Todo\Entity', 'restore']);
+$userIds = [];
+// $todosからuserリストを取得
+foreach ($todos as $todo) {
+    $userIds[] = $todo->getAssigneeId();
+}
+// 重複を省く
+$userIds = array_unique($userIds);
+
+$userRepo = new UserRepository();
+// userを一括で取得する
+$users = $userRepo->getByIds($userIds, ['\App\User\Entity', 'restore']);
 // 仮想的に件数を100倍にする
 for ($i = 0; $i < 100; $i++) {
     foreach ($todos as $todo) {
         $todo->printEntity();
-        $userRepo = new UserRepository();
-        $user     = $userRepo->getById($todo->getAssigneeId(), ['\App\User\Entity', 'restore']);
+        // todoのassigneeをもとにuserを判定
+        $user = $users[$todo->getAssigneeId()];
         $user->printEntity();
         echo '<hr>';
     }
