fbf3ab511cbfc6940533612d3c269f2dd0b3fff7
IkezoeMakoto <150283t@gmail.com>
2021-03-16 06:06:37 +0900

    一旦

---
+++ app/src/3-class-method.php
@@ -0,0 +1,11 @@
+<?php
+class User {
+    private string $id;
+    private string $name;
+    public function __construct(string $name) {
+        $this->id = uniqid('', true);
+        $this->name = $name;
+    }
+}
+$user = new User('山田太郎');
+var_dump($user);
